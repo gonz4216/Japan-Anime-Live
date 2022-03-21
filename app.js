ed0a5048e036141af0c71946da451484a5850eb9
gonz4216 <ethgon19@gmail.com>
2022-03-21 09:39:13 -0400

    main

---
+++ app.js
@@ -0,0 +1,14 @@
+let page = document.getElementById("features")
+function visitAnime(){
+  location.href = "#features"
+}
+
+let discordd = document.getElementById("discord")
+function discord(){
+  alert("Sorry discord isnt up yet")
+}
+
+let logo = document.getElementsByClassName("footer-img")
+  function img() {
+    location.href = "#landing"
+  }
